6972eaf77cab9b8a566a6df293aca35bb30b3859
Daeho <eogh234@naver.com>
2021-02-03 05:19:37 +0900

    배열 Queue 구현

---
+++ app/src/main/java/com/study/week4/ListNodeQueue.java
@@ -0,0 +1,4 @@
+package com.study.week4;
+
+public class ListNodeQueue {
+}
---
+++ app/src/main/java/com/study/week4/Queue.java
@@ -0,0 +1,47 @@
+package com.study.week4;
+
+public class Queue {
+    public int front;
+    public int rear;
+    public int q[];
+
+    public Queue() {
+        front = 0;
+        rear = 0;
+        q = new int[100];
+    }
+
+    public void enqueue(int data){
+        if (rear >= q.length){
+            q = new int[q.length * 2];
+        }
+        rear++;
+        q[rear - 1] = data;
+    }
+
+    public int dequeue(){
+        if (rear - front < 1){
+            System.out.println("Empty Queue");
+            return 0;
+        }
+        int removed = q[front];
+        front++;
+
+        return removed;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
